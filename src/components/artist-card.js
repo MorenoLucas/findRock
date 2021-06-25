0ae59ef06dd793732aa7615d6d2d81f8ea7a5932
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-06-25 12:07:23 +0200

    primer commit

---
+++ src/components/artist-card.js
@@ -0,0 +1,12 @@
+import React from "react";
+class ArtistCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src="https://historico.crazyminds.es/wp-content/uploads/Bandalos-Chinos-Crazyminds.es_.jpg"></img>
+        <p>Banda</p>
+      </div>
+    );
+  }
+}
+export default ArtistCard;
