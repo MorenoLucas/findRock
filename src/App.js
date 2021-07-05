e89cfb97eb5300617646167a91a946f1c5b94319
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-07-05 11:51:29 +0200

    layout footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PageSearchResult from "../src/page-search-result";
 import PageHome from "../src/page-home";
 import PageArtist from "../src/page-artist";
-
+import Footer from "../src/components/footer";
 import "bootstrap/dist/css/bootstrap.css";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -10,12 +10,14 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          {/* para que sea extact  */}
-          <Route exact path="/busqueda" component={PageSearchResult}></Route>
-          <Route path="/artista" component={PageArtist}></Route>
-          <Route path="/" component={PageHome}></Route>
-        </Switch>
+        <Footer>
+          <Switch>
+            {/* para que sea extact  */}
+            <Route exact path="/busqueda" component={PageSearchResult}></Route>
+            <Route path="/artista" component={PageArtist}></Route>
+            <Route path="/" component={PageHome}></Route>
+          </Switch>
+        </Footer>
       </BrowserRouter>
     );
   }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./artist-card.css";
-class Footer extends React.Component {
-  render() {
-    return (
+function Footer(props) {
+  const children = props.children;
+  return (
+    <React.Fragment>
+      {children}
       <div className="row">
         <div className="col-md-12 centrar">
           <p>Copyright Lucky 2021</p>
         </div>
       </div>
-    );
-  }
+    </React.Fragment>
+  );
 }
+
 export default Footer;
