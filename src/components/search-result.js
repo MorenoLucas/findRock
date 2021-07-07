0ef67e4720c1826fa434574314b482acaa49c293
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-07-07 15:00:22 +0200

    consumiendo api sin mostrar

--- src/components/search-result.js
+++ src/components/search-result.js
@@ -2,6 +2,15 @@ import React from "react";
 import ArtistCard from "./artist-card";
 import "bootstrap/dist/css/bootstrap.css";
 class SearchResult extends React.Component {
+  state = { data: "" };
+  componentDidMount() {
+    this.fetchData("https://pokeapi.co/api/v2/pokemon/");
+  }
+  fetchData = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ data: data });
+  };
   render() {
     return (
       <React.Fragment>
