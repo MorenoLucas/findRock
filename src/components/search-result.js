f706f7f12ef6d300d10e8a1a5dc20d2b3531cdb9
MorenoLucas <sl.lucasmoreno@gmail.com>
2021-07-08 11:07:58 +0200

    recuperamos api y la mostramos

--- src/components/search-result.js
+++ src/components/search-result.js
@@ -2,25 +2,38 @@ import React from "react";
 import ArtistCard from "./artist-card";
 import "bootstrap/dist/css/bootstrap.css";
 class SearchResult extends React.Component {
-  state = { data: "" };
+  state = {
+    loading: false,
+    data: { results: [] },
+  };
   componentDidMount() {
-    this.fetchData("https://pokeapi.co/api/v2/pokemon/");
+    this.fetchData("https://pokeapi.co/api/v2/pokemon?limit=151'");
   }
   fetchData = async (url) => {
+    this.setState({ loading: true });
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ data: data });
+    console.log("data", data);
+    this.setState({ loading: false, data: data });
   };
   render() {
     return (
       <React.Fragment>
+        {this.state.loading && <h1>Loading</h1>}
         <div className="container">
           <div className="row">
+            {this.state.data.results.map((item, i) => {
+              return (
+                <ArtistCard
+                  img={`https://pokeres.bastionbot.org/images/pokemon/${
+                    i + 1
+                  }.png`}
+                  title={item.name}
+                  key={i}
+                />
+              );
+            })}
             {/* Props  se ponen como atributos*/}
-            <ArtistCard
-              img="https://www.mehaceruido.com/wp-content/uploads/2020/06/Bandalos-Chinos-slide.jpg"
-              title="Banda Los chinos"
-            />
           </div>
         </div>
       </React.Fragment>
